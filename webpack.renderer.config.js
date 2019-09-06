82e8d9a3e70bc9e5ee7a2e6ea3bfda65cb78a1a0
Jim <jim@advancebass.com>
2019-09-05 22:03:22 -0700

    add audio files and webpack config

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,13 +1,23 @@
-const rules = require('./webpack.rules');
+const rules = require("./webpack.rules");
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+  },
+  {
+    test: /\.(ogg|mp3|wav|mpe?g)$/i,
+    use: "url-loader"
+  },
+  {
+    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+    use: "url-loader?limit=100000"
+  }
+);
 
 module.exports = {
   // Put your normal webpack config below here
   module: {
-    rules,
-  },
+    rules
+  }
 };
