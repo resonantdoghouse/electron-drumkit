daf0100b1f026b218c30c7e26d35ab6ff56850ab
Jim <jim@advancebass.com>
2020-07-14 18:39:47 -0700

    npm audit fix :computer:

--- src/renderer.js
+++ src/renderer.js
@@ -1,18 +1,18 @@
-const Tone = require("tone");
+const Tone = require('tone');
 
-import "./css/index.css";
-import snareSound from "./assets/audio/505/snare.mp3";
-import kickSound from "./assets/audio/505/kick.mp3";
-import highHatSound from "./assets/audio/505/hh.mp3";
-import highHatOpenSound from "./assets/audio/505/hho.mp3";
+import './css/index.css';
+import snareSound from './assets/audio/505/snare.mp3';
+import kickSound from './assets/audio/505/kick.mp3';
+import highHatSound from './assets/audio/505/hh.mp3';
+import highHatOpenSound from './assets/audio/505/hho.mp3';
 
-const $drumKit = document.getElementById("drumkit");
-const $reverbLevel = document.getElementById("reverb-level");
-const $filterLevel = document.getElementById("filter-level");
-const $kick = document.getElementById("kick");
-const $snare = document.getElementById("snare");
-const $hiHat = document.getElementById("hi-hat");
-const $hiHatOpen = document.getElementById("hi-hat-open");
+const $drumKit = document.getElementById('drumkit');
+const $reverbLevel = document.getElementById('reverb-level');
+const $filterLevel = document.getElementById('filter-level');
+const $kick = document.getElementById('kick');
+const $snare = document.getElementById('snare');
+const $hiHat = document.getElementById('hi-hat');
+const $hiHatOpen = document.getElementById('hi-hat-open');
 
 let reverbLevel = 0;
 let filterLevel = 10000;
@@ -24,45 +24,45 @@ let vol = new Tone.Volume(-15);
  * Effects
  */
 const reverb1 = new Tone.Freeverb(reverbLevel, 10000)
-  .receive("reverb")
+  .receive('reverb')
   .toMaster();
-const reverb2 = new Tone.Freeverb(0.4, 10000).receive("reverb").toMaster();
-const reverb3 = new Tone.Freeverb(0.8, 15000).receive("reverb").toMaster();
+const reverb2 = new Tone.Freeverb(0.4, 10000).receive('reverb').toMaster();
+const reverb3 = new Tone.Freeverb(0.8, 15000).receive('reverb').toMaster();
 
-$reverbLevel.addEventListener("input", () => {
+$reverbLevel.addEventListener('input', () => {
   reverbLevel = $reverbLevel.value;
   // console.log(reverbLevel);
   // console.log(reverb1);
   reverb1.roomSize.value = reverbLevel;
 });
 
-$filterLevel.addEventListener("input", () => {
+$filterLevel.addEventListener('input', () => {
   reverb1.dampening.value = $filterLevel.value;
 });
 
-$kick.addEventListener("click", () => {
-  drums505.triggerAttackRelease("C3", "4n");
+$kick.addEventListener('click', () => {
+  drums505.triggerAttackRelease('C3', '4n');
 });
 
-$snare.addEventListener("click", () => {
-  drums505.triggerAttackRelease("D4", "4n");
+$snare.addEventListener('click', () => {
+  drums505.triggerAttackRelease('D4', '4n');
 });
 
-$hiHat.addEventListener("click", () => {
-  drums505.triggerAttackRelease("G3", "4n");
+$hiHat.addEventListener('click', () => {
+  drums505.triggerAttackRelease('G3', '4n');
 });
 
-$hiHatOpen.addEventListener("click", () => {
-  drums505.triggerAttackRelease("A3", "4n");
+$hiHatOpen.addEventListener('click', () => {
+  drums505.triggerAttackRelease('A3', '4n');
 });
 
 /*
  * Delay
  */
 let feedbackDelay = new Tone.PingPongDelay({
-  delayTime: "16n",
+  delayTime: '16n',
   feedback: 0.3,
-  wet: 0.5
+  wet: 0.5,
 }).toMaster();
 
 /*
@@ -73,15 +73,15 @@ const masterCompressor = new Tone.Compressor({
   threshold: -15,
   ratio: 12,
   attack: 0,
-  release: 0.3
+  release: 0.3,
 });
 
 //give a little boost to the lows
 const lowBump = new Tone.Filter({
-  type: "lowshelf",
+  type: 'lowshelf',
   frequency: 90,
   Q: 1,
-  gain: 2
+  gain: 2,
 });
 
 /*
@@ -92,34 +92,34 @@ const drums505 = new Tone.Sampler(
     D4: snareSound,
     C3: kickSound,
     G3: highHatSound,
-    A3: highHatOpenSound
+    A3: highHatOpenSound,
   },
   {
     volume: 1,
-    release: 1
+    release: 1,
   }
 ).chain(vol, reverb1, Tone.Master);
 
 // Route everything through the filter & compressor before playing
 Tone.Master.chain(lowBump, masterCompressor);
 
-document.addEventListener("keydown", e => {
+document.addEventListener('keydown', (e) => {
   // console.log(e.keyCode);
   switch (e.keyCode) {
     case 65:
-      drums505.triggerAttackRelease("C3", "4n"); // kick
+      drums505.triggerAttackRelease('C3', '4n'); // kick
       break;
     case 32:
-      drums505.triggerAttackRelease("C3", "4n");
+      drums505.triggerAttackRelease('C3', '4n');
       break;
     case 83:
-      drums505.triggerAttackRelease("D4", "4n");
+      drums505.triggerAttackRelease('D4', '4n');
       break;
     case 71:
-      drums505.triggerAttackRelease("G3", "4n");
+      drums505.triggerAttackRelease('G3', '4n');
       break;
     case 72:
-      drums505.triggerAttackRelease("A3", "4n");
+      drums505.triggerAttackRelease('A3', '4n');
       break;
     default:
       break;
